62768cd6cab36d5a60eebeaa6fbe7d6adffaed94
Krishana Pal Singh Chouhan <kpsingh.chouhan@gmail.com>
2021-12-05 13:58:36 +0530

    added few vagrant files

---
+++ vagrant/Installation.ps1
@@ -0,0 +1,7 @@
+# Install VirtualBox
+choco install virtualbox
+# Install Vagrant
+choco install vagrant
+
+# Install HostManager Plugin for automatic update of /etc/hosts file
+vagrant plugin install vagrant-hostmanager
